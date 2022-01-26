b4fee1eb0542c5c027e743c564ac8d5eaab68620
qnddjKJH.dev <qnddjKJH.dev@gmail.com>
2022-01-26 18:09:08 +0900

    Init Project

---
+++ src/main/java/com/devjjong/boongboard/BoongboardApplication.java
@@ -0,0 +1,13 @@
+package com.devjjong.boongboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoongboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoongboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devjjong/boongboard/BoongboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjjong.boongboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoongboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
